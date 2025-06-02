dc0db0bdc821dd6de58a4c4cd31873ce6e908dd3
aluno <etecae023@hotmail.com>
2025-06-02 17:11:46 -0300

    first commit

---
+++ models/TaskModel.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+const filePath = "../task.json";
+
+class task {
+    constructor(name, description, status) {
+        this.name = name;
+        this.description = description;
+        this.status = status
+    }
+
+    showTasks(name) {
+
+    }
+
+    createTask() {
+        data = {
+            name: this.name,
+            description: this.description,
+            status: this.status
+        }
+
+        const jsonData = JSON.stringify(data, null, 3);
+        fs.writeFile(filePath, jsonData, "utf8", err => {
+            if(err) {
+                console.log("Error creating task!");
+                return;
+            }
+        });
+
+        console.log("Task created!");
+    }
+}
