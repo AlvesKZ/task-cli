44153c8844cdcacdf110ff83e33221051c72f9c6
aluno <etecae023@hotmail.com>
2025-06-04 16:35:44 -0300

    Write task in a JSON file

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 import read from "readline-sync";
 import fs from "fs";
 
+
 const name = read.question("Enter the project name: ");
 const description = read.question("Enter the project description: ");
 const status = read.question("The project is done? (y/n) ");
 
-if(status !== y || status !== n ) {
-    return (console.log("Awnser the status whith 'y' or 'n'"));
+validate(name, description, status);
+
+const task = {
+    name,
+    description,
+    status,
 }
 
-const createTask = () => {
 
+const taskJson = JSON.stringify(task, null, 2);
+
+const createTask = (json) => {
+    fs.appendFile("./task.json", json, (e) => {
+        if(e) {
+            console.log("Error during create task");
+        }
+    });
+}
+
+createTask(taskJson);
+
+function validate(name, description, status) {
+    if(status !== "y" || status !== "n" ) {
+        console.log("Awnser the status whith 'y' or 'n'");
+        return;
+    }
+
+    if(!name) {
+        console.log("The field 'name' can't be empty");
+        return;
+    }
+    
+    if(!description) {
+        console.log("The field 'name' can't be empty");
+        return;
+    }
 }
